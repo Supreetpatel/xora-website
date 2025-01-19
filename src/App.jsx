ab60a1d97f8bc0ce297367939a0df6af435b47f2
Supreet0 <150177964+Supreetpatel@users.noreply.github.com>
2025-01-20 00:48:25 +0530

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Header from './sections/Header'
+import React from "react";
+import Header from "./sections/Header";
 
 const App = () => {
   return (
     <main className="overflow-hidden">
-      <Header/>
+      <Header />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
